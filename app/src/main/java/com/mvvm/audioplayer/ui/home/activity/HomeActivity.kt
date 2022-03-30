f2ef0c4d739d149414835e180ecd2ee12c8eb7f2
Abhishek <amarpandey7007@gmail.com>
2022-03-30 10:32:23 +0530

    Initial commit

---
+++ app/src/main/java/com/mvvm/audioplayer/ui/home/activity/HomeActivity.kt
@@ -0,0 +1,16 @@
+package com.mvvm.audioplayer.ui.home.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mvvm.audioplayer.R
+import com.mvvm.audioplayer.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+  lateinit  var activityHomeBinding:ActivityHomeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activityHomeBinding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(activityHomeBinding!!.root)
+        activityHomeBinding.tvMohan.text="Nagar"
+    }
+}
---
+++ app/src/main/java/com/mvvm/audioplayer/ui/onboarding/activity/OnBoardingActivity.kt
@@ -0,0 +1,12 @@
+package com.mvvm.audioplayer.ui.onboarding.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mvvm.audioplayer.R
+
+class OnBoardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
