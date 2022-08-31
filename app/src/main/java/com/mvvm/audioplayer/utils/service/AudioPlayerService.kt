097733302140b794889816f3f24daf96e98bbf7a
Abhishek <amarpandey7007@gmail.com>
2022-08-31 07:10:33 +0530

    music service code

--- app/src/main/java/com/mvvm/audioplayer/utils/service/AudioPlayerService.kt
+++ app/src/main/java/com/mvvm/audioplayer/utils/service/AudioPlayerService.kt
@@ -5,21 +5,22 @@ import android.app.Service
 import android.content.Intent
 import android.os.Binder
 import android.os.IBinder
+import com.google.android.exoplayer2.ui.PlayerNotificationManager
+import com.google.android.exoplayer2.util.NotificationUtil.IMPORTANCE_HIGH
 import com.mvvm.audioplayer.R
 
 class AudioPlayerService : Service() {
     private val audioServiceBinder: IBinder = AudioServiceBinder()
-
-        //private lateinit var playerNotificationManager: PlayerNotificationManager
+    private lateinit var playerNotificationManager: PlayerNotificationManager
 
     override fun onCreate() {
         super.onCreate()
-/*        playerNotificationManager =
+        playerNotificationManager =
             PlayerNotificationManager.Builder(applicationContext, NOTIFICATION_ID, CHANNEL_ID)
                 .setChannelImportance(IMPORTANCE_HIGH)
                 .setChannelNameResourceId(R.string.app_name)
                 .setSmallIconResourceId(R.drawable.ic_launcher_foreground)
-                .build()*/
+                .build()
     }
 
     override fun onBind(p0: Intent?): IBinder? {
